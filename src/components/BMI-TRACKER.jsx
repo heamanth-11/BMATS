af4c7243675c14b52b174e2067efcfcde501c518
heamanth-11 <heamanthofficial@gmail.com>
2022-04-20 23:22:13 +0530

    bmi-tracker-ui done

--- src/components/BMI-TRACKER.jsx
+++ src/components/BMI-TRACKER.jsx
@@ -4,15 +4,17 @@ function Bmitracker() {
     return (
      <div className="bmi">
          <div className="bmi-form">
-             <input type="number" placeholder='Weight in KG' name='weight' />
-             <input type="text" name="height" placeholder='height in cm' />
+             <label htmlFor="">BMI TRACKER</label>
+             <input className='side-border weight' type="number" placeholder='Weight in KG' name='weight' />
+             <input className='side-border' type="number" name="height" placeholder='height in cm' />
+             <input className='side-border' type="number" placeholder='Age' name='Age' />
              <select name="gender" id="gender">
              <option value="male">Male</option>
              <option value="female">Female</option>
 
              </select>
-             <input type="number" placeholder='Age' name='Age' />
-            <button id='bmiTracker' type='button'>Estimate BMI</button>
+           
+            <button id='bmiTracker' type='button'>Track BMI</button>
          </div>
         
      </div>
