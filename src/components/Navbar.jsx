02edc78aa7719bf23bf2dca7b2e7d10fdfbcc9ea
heamanth-11 <heamanthofficial@gmail.com>
2022-04-20 21:37:06 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar(props) {
     return (
         <div className="header">
             <nav class="navbar navbar-expand-lg navbar-dark ">
-  <a class="navbar-brand " href="/">BMACS</a>
+  <a class="navbar-brand " href="/">BMATS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
