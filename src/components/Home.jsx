acaf425de4e262463b81b1e6ff9e8dd2497249c6
heamanth-11 <heamanthofficial@gmail.com>
2022-04-26 02:53:46 +0530

    HOME UI REVERSED AS MUSTAQ SUGGESTED

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,7 @@ function Home() {
     
     return (
         <div className='home'>
-            <div className="row">
-                <div className="col-sm6 tracker ">
-                   <Link to="/bmi-tracker"> <button className='bmi-button'>TRACK BMI</button> </Link>
-                </div>
-                <div className="col-sm6  tracker">
-                <Link to="/bmi-tracker">   <button className='bmi-button'> TRACK BMR </button> </Link>
-                </div>
-            </div>
-            <hr/>
+        
                 <div className="plans">
                     <div className="row">
 
@@ -25,9 +17,17 @@ function Home() {
                     <div className="col-sm-4 col-12 plan">
                         <button className='plan-btn'>Weight Loss</button>
                     </div>
-
+                    <hr/>
                     </div>
-
+                    <div className="row">
+                         <div className="col-sm6 tracker ">
+                          <Link to="/bmi-tracker"> <button className='bmi-button'>TRACK BMI</button> </Link>
+                          </div>
+                        <div className="col-sm6  tracker">
+                         <Link to="/bmi-tracker">   <button className='bmi-button'> TRACK BMR </button> </Link>
+                         </div>
+                     </div>
+          
                   x
             </div>
         
