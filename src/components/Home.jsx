45d2d8ee7708ff217966518371f1cce641609420
heamanth-11 <heamanthofficial@gmail.com>
2022-04-25 13:58:49 +0530

    BMR Tracker UI DONE

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 function Home() {
-    onclick=()=>{
-        // alert('HO HO HO ')
-    }
+    
     return (
         <div className='home'>
             <div className="row">
                 <div className="col-sm6 tracker ">
-                   <Link to="/bmi-tracker"> <button onClick={onclick} className='bmi-button'>TRACK BMI</button> </Link>
+                   <Link to="/bmi-tracker"> <button className='bmi-button'>TRACK BMI</button> </Link>
                 </div>
                 <div className="col-sm6  tracker">
                 <Link to="/bmi-tracker">   <button className='bmi-button'> TRACK BMR </button> </Link>
